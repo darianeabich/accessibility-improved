5bdc15a6ae06346053155e5e72e0c6b207581253
Dariane Ramos Abich <dariane.abich@dbsystem.com.br>
2024-02-21 17:19:43 -0300

    feat: add close dialog with ESC and overlay click

--- src/app/pages/modal/modal.component.ts
+++ src/app/pages/modal/modal.component.ts
@@ -1,6 +1,12 @@
 import { A11yModule } from '@angular/cdk/a11y';
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  HostListener,
+  Input,
+  Output,
+} from '@angular/core';
 
 import { Livro } from '../../models/interfaces';
 
@@ -17,6 +23,12 @@ export class ModalComponent {
   @Output() mudouModal = new EventEmitter<boolean>();
 
   constructor() {}
+
+  @HostListener('document:keydown.escape') fecharModalComEsc() {
+    if (this.statusModal) {
+      this.fecharModal();
+    }
+  }
 
   fecharModal() {
     this.statusModal = false;
